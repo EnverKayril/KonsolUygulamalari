48799d5f5f08d443a24da20a3bcc4b62da899882
Envercan Kayril <eckayril@gmail.com>
2024-02-07 16:24:58 +0300

    Add project files.

---
+++ BookOrder/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace BookOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Birim Fiyatları
+            int siir = 18;
+            int cocuk = 15;
+            int roman = 22;
+            int tarih = 20;
+            int felsefe = 17;
+            int biyografi = 14;
+            int secim = 0;
+            int adet = 0;
+            int toplam = 0;
+
+
+            Console.WriteLine("Hoşgeldiniz. Lütfen almak istediğiniz ürünü seçiniz.");
+            Console.WriteLine("1. Şiir \n2. Çocuk \n3. Roman \n4. Tarih \n5. Felsefe \n6. Biyografi \nSiparişi Tamamlamak için '8'e Basınız. \nSipariş Vermeden Çıkış Yapmak İçin '9'a Basınız. \n ");
+
+            while (secim != 9 && secim != 8)
+            {
+                try
+                {
+                    Console.Write("Seçim Yapınız :");
+                    secim = Convert.ToInt32(Console.ReadLine());
+
+                    switch (secim)
+                    {
+                        case 1:
+                            Console.Write("Şiir Kitabı İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + siir * adet);
+                            toplam = toplam + siir * adet;
+                            break;
+
+                        case 2:
+                            Console.Write("Çocuk Kitabı İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + cocuk * adet);
+                            toplam = toplam + cocuk * adet;
+                            break;
+
+                        case 3:
+                            Console.Write("Roman İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + roman * adet);
+                            toplam = toplam + roman * adet;
+                            break;
+
+                        case 4:
+                            Console.Write("Tarih Kitabı İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + tarih * adet);
+                            toplam = toplam + tarih * adet;
+                            break;
+
+                        case 5:
+                            Console.Write("Felsefe Kitabı İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + felsefe * adet);
+                            toplam = toplam + felsefe * adet;
+                            break;
+
+                        case 6:
+                            Console.Write("Biyografi Kitabı İçin Adet Giriniz: ");
+                            adet = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Ara Toplam : " + biyografi * adet);
+                            toplam = toplam + biyografi * adet;
+                            break;
+
+                        case 8:
+                            if (toplam < 400)
+                            {
+                                Console.WriteLine("Toplam : " + (toplam * 0.95) * 0.90);
+                                Console.WriteLine("KDV : " + (toplam * 0.95) * 0.10);
+                                Console.WriteLine("Genel Toplam :" + toplam * 0.95);
+                            }
+                            else if (toplam >= 400 && toplam < 1000)
+                            {
+                                Console.WriteLine("Toplam : " + (toplam * 0.90) * 0.90);
+                                Console.WriteLine("KDV : " + (toplam * 0.90) * 0.10);
+                                Console.WriteLine("Genel Toplam :" + toplam * 0.90);
+                            }
+                            else if (toplam >= 1000 && toplam < 2000)
+                            {
+                                Console.WriteLine("Toplam : " + (toplam * 0.85) * 0.90);
+                                Console.WriteLine("KDV : " + (toplam * 0.85) * 0.10);
+                                Console.WriteLine("Genel Toplam :" + toplam * 0.85);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Toplam : " + (toplam * 0.75) * 0.90);
+                                Console.WriteLine("KDV : " + (toplam * 0.75) * 0.10);
+                                Console.WriteLine("Genel Toplam :" + toplam * 0.75);
+                            }
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hatalı değer girdiniz.");
+                }
+
+            }
+            //Dışarıdan sipariş alınacak olan kitap miktarı girilsin. Sipariş sayısı 20 den az ise toplam ücretten %5 düşürülsün. 20-50 aralığında ise %10. 50-100 aralığında ise %15, 100 den fazla ise %25 indirim yapılsın. Kitabın birim fiyatı 20TL
+        }
+    }
+}
---
+++ Hesap Makinesi/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Hesap_Makinesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hesap Makinesi Uygulaması\n");
+            Console.WriteLine("İşlem : [+][-][*][/] \nSıfırlamak için '8' \n Çıkış için '9'");
+            double sayi1 = 0;
+            double sayi2 = 0;
+            double sayi3 = 0;
+            string mathOp = "";
+
+            try
+            {
+                Console.Write("1. Sayıyı Giriniz : ");
+                sayi1 = Convert.ToDouble(Console.ReadLine());
+                sayi1 = 0 + sayi1;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Hatalı Değer Girdiniz.");
+            }
+
+            while (mathOp != "9")
+            {
+                try
+                {
+                    Console.Write("Operatör Giriniz : ");
+                    mathOp = Console.ReadLine();
+                    Console.Write("2. Sayıyı Giriniz :");
+                    sayi2 = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Hatalı Değer Girdiniz");
+                }
+
+                switch (mathOp)
+                {
+                    case "+":
+                        sayi1 = sayi1 + sayi2;
+                        Console.WriteLine(sayi1);
+                        break;
+
+                    case "-":
+                        sayi1 = sayi1 - sayi2;
+                        Console.WriteLine(sayi1);
+                        break;
+
+                    case "*":
+                        sayi1 = sayi1 * sayi2;
+                        Console.WriteLine(sayi1);
+                        break;
+
+                    case "/":
+                        sayi1 = sayi1 / sayi2;
+                        Console.WriteLine(sayi1);
+                        break;
+
+                    case "8":
+                        sayi1 = 1;
+                        Console.WriteLine("Sıfırlandı." + sayi1);
+                        break;
+
+                    case "9":
+                        Console.WriteLine("Çıkış Yapıldı.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Geçerli Bir Operatör Tanımı Giriniz.");
+                        break;
+
+                }
+
+            }
+        }
+    }
+}
