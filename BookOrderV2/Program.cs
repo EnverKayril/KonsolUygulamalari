eece5323f861223b9bed27a88343f2d21ed56939
Enver Kayrıl <eckayril@gmail.com>
2024-02-19 22:39:34 +0300

    Update

--- BookOrderV2/Program.cs
+++ BookOrderV2/Program.cs
@@ -6,91 +6,93 @@ namespace BookOrderV2
     {
         static void Main(string[] args)
         {
+            List<string> billingInformation = new List<string>();
             double total = 0;
-            double tax = 0;
-            double Kdv = 0.10;
-            
-            List<string> fatura = new List<string>();
-            
 
-            Console.WriteLine("Hoşgeldiniz. Lütfen almak istediğiniz ürünü seçiniz.");
-
-
-            Console.WriteLine(PrintList());
+            Welcome();
 
             bool isRun = true;
-
             while (isRun)
             {
-                Console.Write("Seçim Yapınız: ");
-                int secim = Convert.ToInt32(Console.ReadLine());
-                
-                if (secim == 8)
+                Console.WriteLine("Lütfen almak istediğiniz ürünü seçiniz. \nÜrünleri görüntülemek için '8'e basınız. \nSiparişi tamamlamak için '9'a basınız.");
+
+                int selection = Convert.ToInt32(Console.ReadLine());
+
+                if (selection == 8)
                 {
-                    Console.WriteLine("KITAP ADI  \t\t\t ADET \t  BİRİM FİYATI  \t  ARA TOPLAM");
-                    foreach (string kalem in fatura)
+                    Console.WriteLine(PrintList());
+                    continue;
+                }
+
+                if (selection == 9)
+                {
+                    Console.WriteLine("KİTAP ADI        ADET  BİRİM FİYAT   ARA TOPLAM");
+                    foreach (string billing in billingInformation)
                     {
-                        Console.WriteLine(kalem);
+                        Console.WriteLine(billing);
                     }
-                    Console.WriteLine("Total : " + total);
+                    Console.WriteLine("GENEL TOPLAM : " + total);
                     isRun = false;
                     break;
                 }
-                //PADLEFT
-                Console.WriteLine("Adet giriniz: ");
-                uint adet = Convert.ToUInt32(Console.ReadLine());
+                Console.Write("Adet giriniz: ");
+                uint piece = Convert.ToUInt32(Console.ReadLine());
 
-                
-                total += addFatura(fatura,secim,adet);
-                
-
-
+                total += AddBill(billingInformation, selection, piece);
             }
 
 
 
 
 
+
+            //Dışarıdan sipariş alınacak olan kitap miktarı girilsin. Sipariş sayısı 20 den az ise toplam ücretten %5 düşürülsün. 20-50 aralığında ise %10. 50-100 aralığında ise %15, 100 den fazla ise %25 indirim yapılsın. Kitabın birim fiyatı 20TL
         }
 
-        private static double addFatura( List<string> x, int secim, uint adet)
+        private static void Welcome()
         {
-            double aratoplam = OrderBook(secim, adet);
-            double singlePrice = PriceList()[secim - 1];
-            string kitapAdi = Books()[secim - 1];
-            string kalem = kitapAdi + " \t\t\t" + adet + "\t " + singlePrice +"\t " + aratoplam;
-            x.Add(kalem);
-            return aratoplam;
+            Console.WriteLine("Kitap Sipariş Programı\n");
+            Console.WriteLine("Hoşgeldiniz.");
+            Console.WriteLine(PrintList());
+        }
+
+        private static double AddBill(List<string> bill, int selection, uint piece)
+        {
+            double subTotal = SubTotal(piece, selection);
+            string bookName = BooksList()[selection - 1];
+            double singlePrice = BooksPrice()[selection - 1];
+            string item = (bookName) + " " + (piece) + " " + (singlePrice) + " " + (subTotal);
+            bill.Add(item);
+            return subTotal;
+        }
+
+        private static double SubTotal(uint piece, int selection)
+        {
+            double bookPrice = BooksPrice()[selection - 1];
+            double subTotal = bookPrice * piece;
+            Console.WriteLine(subTotal);
+            return subTotal;
         }
 
         private static string PrintList()
         {
-            string[] nameList = Books();
-            double[] priceList = PriceList();
-            
-            string text = "";
+            string[] bookList = BooksList();
+            double[] priceList = BooksPrice();
+            string list = "";
 
-            for (int i = 0; i < nameList.Length; i++)
+            for (int i = 0; i < bookList.Length; i++)
             {
-                text = text + (i + 1) + ". " + nameList[i] + "  -  " + priceList[i] + "\n";
+                list = list + (i + 1) + ". " + bookList[i] + " -> " + priceList[i] + "\n";
             }
-            return text;
+            return list;
         }
 
-        private static double OrderBook(int secim, uint adet)
+        private static string[] BooksList()
         {
-            double bookPrice = PriceList()[secim - 1];
-            double aratoplam = bookPrice * adet;
-            Console.WriteLine(aratoplam);
-            return aratoplam;
+            return new string[] { "Şiir Kitabı     ", "Çocuk Kitabı    ", "Roman           ", "Tarih Kitabı    ", "Felsefe Kitabı  ", "Biyografi Kitabı" };
         }
 
-        private static string[] Books()
-        {
-            return new string[] { "Şiir Kitabı", "Çocuk Kitabı", "Roman", "Tarih Kitabı", "Felsefe Kitabı", "Biyografi Kitabı" };
-        }
-
-        private static double[] PriceList()
+        private static double[] BooksPrice()
         {
             return new double[] { 18, 15, 22, 20, 17, 14 };
         }
---
+++ EK Kagitcilik/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Security.AccessControl;
+
+namespace EK_Kagitcilik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> faturaDetayı = new List<string>();
+            double total = 0;
+
+            Console.WriteLine("Kağıt Sipariş Sistemi\n");
+            
+
+
+            bool isRun = true;
+            while (isRun)
+            {
+                Console.WriteLine(Kagıtlar() + "Siparişi tamamlamak için '9'a basınız.");
+                Console.Write("\nKağıt Cinsini Seçiniz: ");
+                int secim1 = Convert.ToInt32(Console.ReadLine());
+                if (secim1 == 9) 
+                {
+                    Console.WriteLine("KAĞIT CİNSİ \t  M2 FİYATI\tKAĞIT ENİ \t TOP METRAJI \t\t TOPLAM");
+                    foreach (string kalem in faturaDetayı)
+                    {
+                        Console.WriteLine(kalem);
+                    }
+                    Console.WriteLine("\n\t\t\t\t\t   GENEL TOPLAM  \t\t" + Math.Round(total));
+                    isRun = false; 
+                    break;
+                }
+
+                Console.WriteLine(KagitEnleri());
+                Console.Write("Kağıt Enini Seçiniz: ");
+                int secim2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(TopMetrajlari());
+                Console.Write("Top Metrajını Seçiniz: ");
+                int secim3 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Adet Giriniz: ");
+                uint adet = Convert.ToUInt32(Console.ReadLine());
+
+                total += Fatura(faturaDetayı, secim1, secim2, secim3, adet);
+            }
+        }
+
+
+
+
+        private static double Fatura(List<string>fis, int secim1, int secim2, int secim3, uint adet)
+        {
+            string kagitCinsi = KagıtCinsi()[secim1 - 1];
+            double metrekareFiyatı = MetrekareFiyatı()[secim1 - 1];
+            double kagitEni = KagıtEni()[secim2 - 1];
+            double topMetraji = TopMetraji()[secim3 - 1];
+            double araToplam = AraToplam(secim1, secim2, secim3, adet);
+            string item = ("\t" + kagitCinsi + "\t\t" + metrekareFiyatı + "\t    " + kagitEni + "\t\t\t" + topMetraji + "\t\t" + araToplam);
+            fis.Add(item);
+            return Math.Round(araToplam,2);
+        }
+
+        private static double AraToplam(int secim1, int secim2,int secim3, uint adet )
+        {
+            double metrekareFiyati = MetrekareFiyatı()[secim1 - 1];
+            double kagitEni = KagıtEni()[secim2 - 1];
+            double topMetraji = TopMetraji()[secim3 - 1];
+            double araToplam = metrekareFiyati * kagitEni * topMetraji * adet;
+            Console.WriteLine(Math.Round(araToplam, 2));
+            return Math.Round(araToplam, 2);
+        }
+
+        private static string[] KagıtCinsi()
+        {
+            return new string[] { "s37", "c40", "a50" };
+        }
+        private static double[] MetrekareFiyatı()
+        {
+            return new double[] { 2.40, 1.80, 2.60 };
+        }
+        private static double[] KagıtEni()
+        {
+            return new double[] { 1.00, 1.60, 1.80, 2.20, 3.20 };
+        }
+        private static double[] TopMetraji()
+        {
+            return new double[] { 300, 500, 700 };
+        }
+
+        private static string Kagıtlar()
+        {
+            string[] kagıtCinsi = KagıtCinsi();
+            double[] metrekareFiyat = MetrekareFiyatı();
+            string list = "";
+            for (int i = 0; i < kagıtCinsi.Length; i++)
+            {
+                list = list + (i + 1) + ". " + kagıtCinsi[i] + " -> " + metrekareFiyat[i] +"\n";
+            }
+            return list;
+        }
+
+        private static string KagitEnleri()
+        {
+            double[] kagitEni = KagıtEni();
+            string list = "";
+            for (int i = 0;i < kagitEni.Length;i++)
+            {
+                list = list + (i + 1) + ". " + kagitEni[i] + "\n";
+            }
+            return list;
+        }
+
+        private static string TopMetrajlari()
+        {
+            double[] topMetraj = TopMetraji();
+            string list = "";
+            for (int i = 0; i < topMetraj.Length; i++) 
+            {
+                list = list + (i + 1) + ". " + topMetraj[i] + "\n";
+            }
+            return list;
+        }
+
+
+    }
+}
